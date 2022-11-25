d6bec7ab55c12b46681ef687460b4184246b1ed5
Unknown <113817466+userNotAvailableE@users.noreply.github.com>
2022-11-25 13:58:52 +0530

    Add files via upload

---
+++ Boxing.java
@@ -0,0 +1,23 @@
+package com.boxing;
+
+public class Boxing {
+    public static void main(String[] args) {
+        int i = 5; // primitives are fast
+        Integer ii = new Integer(i);  // boxing wrapping
+
+        int j = ii.intValue(); // unboxing unwrapping
+
+        Integer value = i; // Auto boxing
+
+        int k = value;  // Auto unboxing
+
+        System.out.println(i + " " + ii + " " + value + " " + k);
+        // runs without error
+
+        String str = "123";
+        // int n = str; // error
+        int n = Integer.parseInt(str);
+        System.out.println("After parseInt " + n);
+
+    }
+}
